74bbce957997a90502174abe1466d4f1afeeccbd
Jose Luis Soto <jose.soto.h@gmail.com>
2024-08-04 13:41:43 -0600

    update header

--- front_end.py
+++ front_end.py
@@ -2,12 +2,15 @@ import backend
 import streamlit as st
 from streamlit_chat import message
 
-st.html(
-    "<div style='display: flex; justify-content: space-between;'><div><h1>Chat Modelo de Ventas</h1><span>Puedes realizarme preguntas relacionadas a los materiales existentes</span></div><div><img width='130' src='https://ideeo.mx/bbva/chatbot/img/chat.png'></div></div>"
+st.markdown(
+    """
+ <header style='padding:20px 0;background-color: #072146;color: #fff;'> <div style='width: 600px; margin: 0 auto;'><img src='https://ideeo.mx/bbva/chatbot/img/logoBBVA.png' style='width: 120px;'></div></header>
+""",
+    unsafe_allow_html=True,
 )
 
 st.html(
-    " <header style='padding:20px 0;background-color: #072146;color: #fff;'> <div style='width: 600px; margin: 0 auto;'><img src='https://ideeo.mx/bbva/chatbot/img/logoBBVA.png' style='width: 120px;'></div></header>"
+    "<div style='display: flex; justify-content: space-between;'><div><h1>Chat Modelo de Ventas</h1><span>Puedes realizarme preguntas relacionadas a los materiales existentes</span></div><div><img width='130' src='https://ideeo.mx/bbva/chatbot/img/chat.png'></div></div>"
 )
 
 if "preguntas" not in st.session_state:
